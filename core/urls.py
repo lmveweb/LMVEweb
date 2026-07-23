a57ad2be9b2dab200a7e59b9146d10b0f0112826
Mauricio Montenegro <mau.montenegro@duocuc.cl>
2026-07-23 04:47:53 -0400

    MVP a2.0

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('proyecto/', views.proyecto, name='proyecto'),
+    path('impacto/', views.impacto, name='impacto'),
+    path('redes/', views.redes, name='redes'),
+    path('contacto/', views.contacto, name='contacto'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def proyecto(request):
+    return render(request, 'core/base.html')
+
+def impacto(request):
+    return render(request, 'core/base.html')
+
+def redes(request):
+    return render(request, 'core/base.html')
+
+def contacto(request):
+    return render(request, 'core/base.html')
