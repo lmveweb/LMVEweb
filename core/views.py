bafee553ca62e18a9bbe9fea3af22ed7d2bbd4e2
Mauricio Montenegro <mau.montenegro@duocuc.cl>
2026-07-24 03:43:03 -0400

    MVP 2

--- core/views.py
+++ core/views.py
@@ -4,13 +4,13 @@ def home(request):
     return render(request, 'core/home.html')
 
 def proyecto(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/proyecto.html')
 
 def impacto(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/impacto.html')
 
 def redes(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/redes.html')
 
 def contacto(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/contacto.html')
