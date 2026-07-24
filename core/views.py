2306f94ec07c9ae31fc5b236ccfb5c2bc98950a4
Mauricio Montenegro <mau.montenegro@duocuc.cl>
2026-07-24 04:21:16 -0400

    MPV 3

--- core/views.py
+++ core/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def proyecto(request):
+    return render(request, 'core/proyecto.html')
+
+def impacto(request):
+    return render(request, 'core/impacto.html')
+
+def redes(request):
+    return render(request, 'core/redes.html')
+
+def contacto(request):
+    return render(request, 'core/contacto.html')
